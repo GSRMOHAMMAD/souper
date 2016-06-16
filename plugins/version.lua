c28d2d076edd0024580952603f8cb0f4515d15c1
GSRMOHAMMAD <mamadbt19@gmail.com>
2016-06-16 11:54:56 -0800

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,15 +1,15 @@
 do
 
 function run(msg, matches)
-  local text = [[ [SpheroBoT](http://telegram.me/sphero)
+  local text = [[ [koni nazn)
   
 *Version: 2.2*
   
 _more information send 
 !spherobot 
 to chat_
-[Developer](http://telegram.me/mrblacklife)
-[Channel](http://telegram.me/spheroch)
+[Developer](jande kqa)
+[Channel)
 *TNX to all*]]
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
@@ -17,10 +17,10 @@ return {
   description = "Shows bot version", 
   usage = "!version: Shows bot version",
   patterns = {
-    "^[!/]version$",
-    "^[!/]ver$",
-    "^[Vv]ersion$",
-    "^[Vv]er$"
+    "^[>]version$",
+    "^[>]ver$",
+    "^[>]ersion$",
+    "^[>]er$"
   }, 
   run = run 
 }
